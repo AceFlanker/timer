65d7357ef42f661077d1452f39cf7126ed74add7
Yuan (Daniel) Li <86581841+AceFlanker@users.noreply.github.com>
2021-08-24 23:40:02 -0700

    Comment added

--- timer1.js
+++ timer1.js
@@ -8,3 +8,13 @@ for (const element of beepBeep) {
     }, element * 1000);  
   }
 }
+
+// RE: Edge cases
+// When there's no input after "node timer1.js", 
+// no sound will be made from terminal regardless,
+// at least, it is the case on an M1 MacBook.
+
+// And when using process.argv, all terminal
+// inputs will become strings anway, so by default
+// we have to convert all of them anyway. So it's
+// kind of moot to account for these cases.
